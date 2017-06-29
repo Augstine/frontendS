a87d0c564df4149ed9ed862e727b89ec90208ac8
augstine <2382813261@qq.com>
2017-06-29 20:55:25 +0800

    首页模块化处理

--- views/static/js/common.js
+++ views/static/js/common.js
@@ -1,8 +1,9 @@
-
-	NProgress.start();
-
-	NProgress.done();
-
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+define(['jquery','template','cookie'],function($,template){
+	$(function(){
+		if(location.pathname != "/dashboard/login"){
+			var fileInfo = JSON.parse($.cookie('fileinfo'));
+      var html = template("profile-tpl",fileInfo);
+      $("#userinfo").html(html);
+		}
+  })
+})
